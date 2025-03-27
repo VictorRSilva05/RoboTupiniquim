3cbc12b7ff609e19d15c64c64737ee8626e28744
Victor R. Silva <victorr.silva005@gmail.com>
2025-03-27 13:34:29 -0300

    Add project files.

---
+++ RoboTupiniquim.ConsoleApp/Robo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboTupiniquim.ConsoleApp
+{
+    public class Robo
+    {
+        public int PosicaoX {  get; set; } 
+        public int PosicaoY { get; set; }
+        public char Direcao { get; set; }
+
+        public Robo()
+        {
+            PosicaoX = 0;
+            PosicaoY = 0;
+            Direcao = 'N';
+        }
+    }
+}
